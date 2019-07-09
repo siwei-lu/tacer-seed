30e9d1bcd05c60684e13987e5a88a3380f65af67
Idan Loo <siwei.lu@lusiweis-MacBook-Pro.local>
2019-07-09 18:14:10 +0800

    chore: remove useless files

--- .bin/generate.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const { resolve } = require('path')
-const { generate } = require('../dist')
-
-const path = resolve(__dirname, '../template')
-generate('seed', path, process.argv[2])
